313ff99c5a294ef07993da7aefb3c1622415fe67
Vincent Esche <regexident@gmail.com>
2018-05-22 10:34:23 +0200

    Added stack-allocated median filter

---
+++ src/stack.rs
@@ -0,0 +1,545 @@
+//! An implementation of a stack-allocated, efficient O(n) median filter.
+
+use std::fmt;
+
+use arrayvec::{Array, ArrayVec};
+
+macro_rules! filter_def {
+    ($n:ident<$t:ident>: $w:expr) => {
+        /// An implementation of a median filter of fixed width with linear complexity.
+        #[derive(Clone, Debug)]
+        pub struct $n<T> {
+            inner: Filter<[ListNode<$t>; $w]>,
+        }
+
+        impl<T> $n<T>
+        where
+            T: Clone + PartialOrd,
+        {
+            /// Creates a new median filter with a given window size.
+            pub fn new() -> Self {
+                Self {
+                    inner: Filter::new(),
+                }
+            }
+
+            /// Returns the window size of the filter.
+            #[inline]
+            pub fn len(&self) -> usize {
+                self.inner.len()
+            }
+
+            /// Returns the filter buffer's current median value, panicking if empty.
+            #[inline]
+            pub fn median(&self) -> T {
+                self.inner.median()
+            }
+
+            /// Returns the filter buffer's current min value, panicking if empty.
+            #[inline]
+            pub fn min(&self) -> T {
+                self.inner.min()
+            }
+
+            /// Returns the filter buffer's current max value, panicking if empty.
+            #[inline]
+            pub fn max(&self) -> T {
+                self.inner.max()
+            }
+
+            /// Applies a median filter to the consumed value.
+            #[inline]
+            pub fn consume(&mut self, value: T) -> T {
+                self.inner.consume(value)
+            }
+        }
+    }
+}
+
+filter_def!(Filter1<T>: 1);
+filter_def!(Filter2<T>: 2);
+filter_def!(Filter3<T>: 3);
+filter_def!(Filter4<T>: 4);
+filter_def!(Filter5<T>: 5);
+filter_def!(Filter6<T>: 6);
+filter_def!(Filter7<T>: 7);
+filter_def!(Filter8<T>: 8);
+filter_def!(Filter9<T>: 9);
+filter_def!(Filter10<T>: 10);
+filter_def!(Filter11<T>: 11);
+filter_def!(Filter12<T>: 12);
+filter_def!(Filter13<T>: 13);
+filter_def!(Filter14<T>: 14);
+filter_def!(Filter15<T>: 15);
+filter_def!(Filter16<T>: 16);
+filter_def!(Filter17<T>: 17);
+filter_def!(Filter18<T>: 18);
+filter_def!(Filter19<T>: 19);
+filter_def!(Filter20<T>: 20);
+filter_def!(Filter21<T>: 21);
+filter_def!(Filter22<T>: 22);
+filter_def!(Filter23<T>: 23);
+filter_def!(Filter24<T>: 24);
+filter_def!(Filter25<T>: 25);
+filter_def!(Filter26<T>: 26);
+filter_def!(Filter27<T>: 27);
+filter_def!(Filter28<T>: 28);
+filter_def!(Filter29<T>: 29);
+filter_def!(Filter30<T>: 30);
+filter_def!(Filter31<T>: 31);
+filter_def!(Filter32<T>: 32);
+
+#[derive(Clone, PartialEq, Eq)]
+struct ListNode<T> {
+    value: Option<T>,
+    previous: usize,
+    next: usize,
+}
+
+impl<T> fmt::Debug for ListNode<T>
+where
+    T: fmt::Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "@{:?}-{:?}-@{:?}", self.previous, self.value, self.next)
+    }
+}
+
+/// An implementation of a median filter with linear complexity.
+///
+/// While the common naïve implementation of a median filter
+/// has a worst-case complexity of `O(n^2)` (due to having to sort the sliding window)
+/// the use of a combination of linked list and ring buffer allows for
+/// a worst-case complexity of `O(n)`.
+struct Filter<A>
+where
+    A: Array,
+{
+    // Buffer of list nodes:
+    buffer: ArrayVec<A>,
+    // Cursor into circular buffer of data:
+    cursor: usize,
+    // Cursor to beginning of circular list:
+    head: usize,
+    // Cursor to median of circular list:
+    median: usize,
+}
+
+impl<T, A> Clone for Filter<A>
+where
+    T: Clone,
+    A: Clone + Array<Item = ListNode<T>>,
+{
+    fn clone(&self) -> Self {
+        Self {
+            buffer: self.buffer.clone(),
+            cursor: self.cursor.clone(),
+            head: self.head.clone(),
+            median: self.median.clone(),
+        }
+    }
+}
+
+impl<T, A> fmt::Debug for Filter<A>
+where
+    T: fmt::Debug,
+    A: fmt::Debug + Array<Item = ListNode<T>>,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("Filter")
+            .field("buffer", &self.buffer)
+            .field("cursor", &self.cursor)
+            .field("head", &self.head)
+            .field("median", &self.median)
+            .finish()
+    }
+}
+
+impl<T, A> Filter<A>
+where
+    T: Clone + PartialOrd,
+    A: Clone + Array<Item = ListNode<T>>,
+{
+    /// Creates a new median filter with a given window size.
+    pub fn new() -> Self {
+        let mut buffer = ArrayVec::new();
+        let size = buffer.capacity();
+        for i in 0..size {
+            buffer.push(ListNode {
+                value: None,
+                previous: (i + size - 1) % size,
+                next: (i + 1) % size,
+            });
+        }
+        Filter {
+            buffer: buffer,
+            cursor: 0,
+            head: 0,
+            median: 0,
+        }
+    }
+
+    /// Returns the window size of the filter.
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.buffer.len()
+    }
+
+    /// Returns the filter buffer's current median value, panicking if empty.
+    #[inline]
+    pub fn median(&self) -> T {
+        assert!(!self.buffer.is_empty());
+
+        unsafe { self.read_median() }
+    }
+
+    /// Returns the filter buffer's current min value, panicking if empty.
+    #[inline]
+    pub fn min(&self) -> T {
+        assert!(!self.buffer.is_empty());
+
+        unsafe { self.read_min() }
+    }
+
+    /// Returns the filter buffer's current max value, panicking if empty.
+    #[inline]
+    pub fn max(&self) -> T {
+        assert!(!self.buffer.is_empty());
+
+        unsafe { self.read_max() }
+    }
+
+    /// Applies a median filter to the consumed value.
+    ///
+    /// # Implementation
+    ///
+    /// The algorithm makes use of a ring buffer of the same size as its filter window.
+    /// Inserting values into the ring buffer appends them to a linked list that is *embedded*
+    /// inside said ring buffer (using relative integer jump offsets as links).
+    ///
+    /// # Example
+    ///
+    /// Given a sequence of values `[3, 2, 4, 6, 5, 1]` and a buffer of size 5,
+    /// the buffer would be filled like this:
+    ///
+    /// ```plain
+    /// new(5)  consume(3)  consume(2)  consume(4)  consume(6)  consume(5)  consume(1)
+    /// ▶︎[ ]      ▷[3]       ┌→[3]       ┌→[3]─┐     ┌→[3]─┐    ▶︎┌→[3]─┐      ▷[1]─┐
+    ///  [ ]      ▶︎[ ]      ▷└─[2]      ▷└─[2] │    ▷└─[2] │    ▷└─[2] │    ▶︎┌─[2]←┘
+    ///  [ ]       [ ]        ▶︎[ ]         [4]←┘     ┌─[4]←┘     ┌─[4]←┘     └→[4]─┐
+    ///  [ ]       [ ]         [ ]        ▶︎[ ]       └→[6]       │ [6]←┐     ┌→[6] │
+    ///  [ ]       [ ]         [ ]         [ ]        ▶︎[ ]       └→[5]─┘     └─[5]←┘
+    /// ```
+    ///
+    /// # Algorithm
+    ///
+    /// 1. **Remove node** at current cursor (`▶︎`) from linked list, if it exists.
+    ///    (by re-wiring its predecessor to its successor).
+    /// 2. **Initialize** `current` and `median` index to first node of linked list (`▷`).
+    /// 3. **Walk through** linked list, **searching** for insertion point.
+    /// 4. **Shift median index** on every other hop (thus ending up in the list's median).
+    /// 5. **Insert value** into ring buffer and linked list respectively.
+    /// 6. **Update index** to linked list's first node, if necessary.
+    /// 7. **Update ring buffer**'s cursor.
+    /// 8. **Return median value**.
+    ///
+    /// (_Based on Phil Ekstrom, Embedded Systems Programming, November 2000._)
+
+    pub fn consume(&mut self, value: T) -> T {
+        // If the current head is about to be overwritten
+        // we need to make sure to have the head point to
+        // the next node after the current head:
+        unsafe { self.move_head_forward(); }
+
+        // Remove the node that is about to be overwritten
+        // from the linked list:
+        unsafe { self.remove_node(); }
+
+        // Initialize `self.median` pointing
+        // to the first (smallest) node in the sorted list:
+        unsafe { self.initialize_median(); }
+
+        // Search for the insertion index in the linked list
+        // in regards to `value` as the insertion index.
+        unsafe { self.insert_value(&value); }
+
+        // Update head to newly inserted node if
+        // cursor's value <= head's value or head is empty:
+        unsafe { self.update_head(&value); }
+
+        // If the filter has an even window size, then shift the median
+        // back one slot, so that it points to the left one
+        // of the middle pair of median values
+        unsafe { self.adjust_median_for_even_length(); }
+
+        // Increment and wrap data in pointer:
+        unsafe { self.increment_cursor(); }
+
+        // Read node value from buffer at `self.medium`:
+        unsafe { self.read_median() }
+    }
+
+    #[inline]
+    fn should_insert(&self, value: &T, current: usize, index: usize) -> bool {
+        if let Some(ref v) = self.buffer[current].value {
+            (index + 1 == self.len()) || !(v < value)
+        } else {
+            true
+        }
+    }
+
+    #[inline]
+    unsafe fn move_head_forward(&mut self) {
+        if self.cursor == self.head {
+            self.head = self.buffer[self.head].next;
+        }
+    }
+
+    #[inline]
+    unsafe fn remove_node(&mut self) {
+        let (predecessor, successor) = {
+            let node = &self.buffer[self.cursor];
+            (node.previous, node.next)
+        };
+        self.buffer[predecessor].next = successor;
+        self.buffer[self.cursor] = ListNode {
+            previous: usize::max_value(),
+            value: None,
+            next: usize::max_value(),
+        };
+        self.buffer[successor].previous = predecessor;
+    }
+
+    #[inline]
+    unsafe fn initialize_median(&mut self) {
+        self.median = self.head;
+    }
+
+    #[inline]
+    unsafe fn insert_value(&mut self, value: &T) {
+        let mut current = self.head;
+        let buffer_len = self.len();
+        let mut has_inserted = false;
+        for index in 0..buffer_len {
+            if !has_inserted {
+                let should_insert = self.should_insert(value, current, index);
+                if should_insert {
+                    // Insert previously removed node with new value
+                    // into linked list at given insertion index.
+                    self.insert(value, current);
+                    has_inserted = true;
+                }
+            }
+
+            // Shift median on every other element in the list,
+            // so that it ends up in the middle, eventually:
+            self.shift_median(index, current);
+
+            current = self.buffer[current].next;
+        }
+    }
+
+    #[inline]
+    unsafe fn insert(&mut self, value: &T, current: usize) {
+        let successor = current;
+        let predecessor = self.buffer[current].previous;
+        debug_assert!(self.buffer.len() == 1 || current != self.cursor);
+        self.buffer[predecessor].next = self.cursor;
+        self.buffer[self.cursor] = ListNode {
+            previous: predecessor,
+            value: Some(value.clone()),
+            next: successor,
+        };
+        self.buffer[successor].previous = self.cursor;
+    }
+
+    #[inline]
+    unsafe fn shift_median(&mut self, index: usize, current: usize) {
+        if (index & 0b1 == 0b1) && (self.buffer[current].value.is_some()) {
+            self.median = self.buffer[self.median].next;
+        }
+    }
+
+    #[inline]
+    unsafe fn update_head(&mut self, value: &T) {
+        let should_update_head = if let Some(ref head) = self.buffer[self.head].value {
+            value <= head
+        } else {
+            true
+        };
+
+        if should_update_head {
+            self.head = self.cursor;
+            self.median = self.buffer[self.median].previous;
+        }
+    }
+
+    #[inline]
+    unsafe fn adjust_median_for_even_length(&mut self) {
+        if self.len() % 2 == 0 {
+            self.median = self.buffer[self.median].previous;
+        }
+    }
+
+    #[inline]
+    unsafe fn increment_cursor(&mut self) {
+        self.cursor = (self.cursor + 1) % (self.len());
+    }
+
+    #[inline]
+    unsafe fn read_median(&self) -> T {
+        let index = self.median;
+        self.buffer[index].value.clone().unwrap()
+    }
+
+    #[inline]
+    unsafe fn read_min(&self) -> T {
+        let index = self.head;
+        self.buffer[index].value.clone().unwrap()
+    }
+
+    #[inline]
+    unsafe fn read_max(&self) -> T {
+        let index = (self.cursor + self.len() - 1) % (self.len());
+        self.buffer[index].value.clone().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! test_filter {
+        ($size:expr, $input:expr, $output:expr) => {
+            let filter: Filter<[ListNode<_>; $size]> = Filter::new();
+            let output: Vec<_> = $input.iter().scan(filter, |filter, &input| {
+                Some(filter.consume(input))
+            }).collect();
+            assert_eq!(output, $output);
+        }
+    }
+
+    #[test]
+    fn single_peak_4() {
+        let input = vec![10, 20, 30, 100, 30, 20, 10];
+        let output = vec![10, 10, 20, 20, 30, 30, 20];
+
+        test_filter!(4, input, output);
+    }
+
+    #[test]
+    fn single_peak_5() {
+        let input = vec![10, 20, 30, 100, 30, 20, 10];
+        let output = vec![10, 10, 20, 20, 30, 30, 30];
+        test_filter!(5, input, output);
+    }
+
+    #[test]
+    fn single_valley_4() {
+        let input = vec![90, 80, 70, 10, 70, 80, 90];
+        let output = vec![90, 80, 80, 70, 70, 70, 70];
+        test_filter!(4, input, output);
+    }
+
+    #[test]
+    fn single_valley_5() {
+        let input = vec![90, 80, 70, 10, 70, 80, 90];
+        let output = vec![90, 80, 80, 70, 70, 70, 70];
+        test_filter!(5, input, output);
+    }
+
+    #[test]
+    fn single_outlier_4() {
+        let input = vec![10, 10, 10, 100, 10, 10, 10];
+        let output = vec![10, 10, 10, 10, 10, 10, 10];
+        test_filter!(4, input, output);
+    }
+
+    #[test]
+    fn single_outlier_5() {
+        let input = vec![10, 10, 10, 100, 10, 10, 10];
+        let output = vec![10, 10, 10, 10, 10, 10, 10];
+        test_filter!(5, input, output);
+    }
+
+    #[test]
+    fn triple_outlier_4() {
+        let input = vec![10, 10, 100, 100, 100, 10, 10];
+        let output = vec![10, 10, 10, 10, 100, 100, 10];
+        test_filter!(4, input, output);
+    }
+
+    #[test]
+    fn triple_outlier_5() {
+        let input = vec![10, 10, 100, 100, 100, 10, 10];
+        let output = vec![10, 10, 10, 10, 100, 100, 100];
+        test_filter!(5, input, output);
+    }
+
+    #[test]
+    fn quintuple_outlier_4() {
+        let input = vec![10, 100, 100, 100, 100, 100, 10];
+        let output = vec![10, 10, 100, 100, 100, 100, 100];
+        test_filter!(4, input, output);
+    }
+
+    #[test]
+    fn quintuple_outlier_5() {
+        let input = vec![10, 100, 100, 100, 100, 100, 10];
+        let output = vec![10, 10, 100, 100, 100, 100, 100];
+        test_filter!(5, input, output);
+    }
+
+    #[test]
+    fn alternating_4() {
+        let input = vec![10, 20, 10, 20, 10, 20, 10];
+        let output = vec![10, 10, 10, 10, 10, 10, 10];
+        test_filter!(4, input, output);
+    }
+
+    #[test]
+    fn alternating_5() {
+        let input = vec![10, 20, 10, 20, 10, 20, 10];
+        let output = vec![10, 10, 10, 10, 10, 20, 10];
+        test_filter!(5, input, output);
+    }
+
+    #[test]
+    fn ascending_4() {
+        let input = vec![10, 20, 30, 40, 50, 60, 70];
+        let output = vec![10, 10, 20, 20, 30, 40, 50];
+        test_filter!(4, input, output);
+    }
+
+    #[test]
+    fn ascending_5() {
+        let input = vec![10, 20, 30, 40, 50, 60, 70];
+        let output = vec![10, 10, 20, 20, 30, 40, 50];
+        test_filter!(5, input, output);
+    }
+
+    #[test]
+    fn descending_4() {
+        let input = vec![70, 60, 50, 40, 30, 20, 10];
+        let output = vec![70, 60, 60, 50, 40, 30, 20];
+        test_filter!(4, input, output);
+    }
+
+    #[test]
+    fn descending_5() {
+        let input = vec![70, 60, 50, 40, 30, 20, 10];
+        let output = vec![70, 60, 60, 50, 50, 40, 30];
+        test_filter!(5, input, output);
+    }
+
+    #[test]
+    fn min_max_median() {
+        let mut filter = Filter5::new();
+        for input in vec![70, 50, 30, 10, 20, 40, 60] {
+            filter.consume(input);
+        }
+        assert_eq!(filter.min(), 10);
+        assert_eq!(filter.max(), 60);
+        assert_eq!(filter.median(), 30);
+    }
+}
