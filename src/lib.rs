1141f9638fabb6000344ab1f2f8bb03953c68c4e
Vincent Esche <regexident@gmail.com>
2018-07-10 12:17:07 +0200

    Improved support for `no_std`

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
-#![cfg_attr(not(any(feature = "std", test)), no_std)]
-#![warn(missing_docs)]
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 //! An implementation of an efficient O(n) median filter.
 
-#[cfg(not(any(feature = "std", test)))]
+#![warn(missing_docs)]
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(not(feature = "std"))]
 extern crate core as std;
+
+#[cfg(not(feature = "std"))]
+#[macro_use]
+extern crate std;
 
 extern crate generic_array;
 
